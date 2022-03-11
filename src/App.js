fb8b1da38a9e38772faabd9c20d4059936b577a9
Adrian Barreto Arenaz <abarretoa@unav.es>
2022-03-11 14:22:00 +0100

    v1.0 El crud

--- src/App.js
+++ src/App.js
@@ -1,23 +1,235 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import { Modal, ModalBody, ModalHeader, ModalFooter } from "reactstrap";
+import { v4 as uuidv4 } from "uuid";
 function App() {
+  const dataPaises = [
+    { id: uuidv4(), nombre: "Filipinas", minutos: 241 },
+    { id: uuidv4(), nombre: "Brasil", minutos: 225 },
+    { id: uuidv4(), nombre: "Colombia", minutos: 216 },
+    { id: uuidv4(), nombre: "Nigeria", minutos: 216 },
+    { id: uuidv4(), nombre: "Argentina", minutos: 207 },
+    { id: uuidv4(), nombre: "Indonesia", minutos: 195 },
+    { id: uuidv4(), nombre: "Emiratos Árabes Unidos", minutos: 191 },
+    { id: uuidv4(), nombre: "México", minutos: 190 },
+    { id: uuidv4(), nombre: "Sudáfrica", minutos: 190 },
+    { id: uuidv4(), nombre: "Egipto", minutos: 186 },
+  ];
+
+  const [data, setData] = useState(dataPaises);
+  const [modalEditar, setModalEditar] = useState(false);
+  const [modalEliminar, setModalEliminar] = useState(false);
+  const [modalInsertar, setModalInsertar] = useState(false);
+
+  const [paisSeleccionado, setPaisSeleccionado] = useState({
+    id: "",
+    nombre: "",
+    minutos: "",
+  });
+
+  const seleccionarPais = (elemento, caso) => {
+    setPaisSeleccionado(elemento);
+    caso === "Editar" ? setModalEditar(true) : setModalEliminar(true);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setPaisSeleccionado((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const editar = () => {
+    var dataNueva = data;
+    dataNueva.map((pais) => {
+      if (pais.id === paisSeleccionado.id) {
+        pais.minutos = paisSeleccionado.minutos;
+        pais.nombre = paisSeleccionado.nombre;
+      }
+    });
+    setData(dataNueva);
+    setModalEditar(false);
+  };
+
+  const eliminar = () => {
+    setData(data.filter((pais) => pais.id !== paisSeleccionado.id));
+    setModalEliminar(false);
+  };
+
+  const abrirModalInsertar = () => {
+    setPaisSeleccionado(null);
+    setModalInsertar(true);
+  };
+
+  const insertar = () => {
+    var valorInsertar = paisSeleccionado;
+    valorInsertar.id = uuidv4();
+    var dataNueva = data;
+    dataNueva.push(valorInsertar);
+    setData(dataNueva);
+    setModalInsertar(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>
+        Países en los que la gente pasa más tiempo en redes sociales (2019)
+      </h2>
+      <br />
+      <button
+        className="btn btn-success btn-sm"
+        onClick={() => abrirModalInsertar()}
+      >
+        Insertar
+      </button>
+      <br />
+      <br />
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Minutos (por día)</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((elemento) => (
+            <tr>
+              <td>{elemento.id}</td>
+              <td>{elemento.nombre}</td>
+              <td>{elemento.minutos}</td>
+              <td>
+                <button
+                  className="btn btn-primary btn-sm"
+                  onClick={() => seleccionarPais(elemento, "Editar")}
+                >
+                  Editar
+                </button>{" "}
+                {"   "}
+                <button
+                  className="btn btn-danger btn-sm"
+                  onClick={() => seleccionarPais(elemento, "Eliminar")}
+                >
+                  Eliminar
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <Modal isOpen={modalEditar}>
+        <ModalHeader>
+          <div>
+            <h3>Editar País</h3>
+          </div>
+        </ModalHeader>
+        <ModalBody>
+          <div className="form-group">
+            <label>ID</label>
+            <input
+              className="form-control"
+              readOnly
+              type="text"
+              name="id"
+              value={paisSeleccionado && paisSeleccionado.id}
+            />
+            <br />
+
+            <label>País</label>
+            <input
+              className="form-control"
+              type="text"
+              name="nombre"
+              value={paisSeleccionado && paisSeleccionado.nombre}
+              onChange={handleChange}
+            />
+            <br />
+
+            <label>Minutos</label>
+            <input
+              className="form-control"
+              type="text"
+              name="minutos"
+              value={paisSeleccionado && paisSeleccionado.minutos}
+              onChange={handleChange}
+            />
+            <br />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-primary" onClick={() => editar()}>
+            Actualizar
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={() => setModalEditar(false)}
+          >
+            Cancelar
+          </button>
+        </ModalFooter>
+      </Modal>
+
+      <Modal isOpen={modalEliminar}>
+        <ModalBody>
+          Estás Seguro que deseas eliminar el país{" "}
+          {paisSeleccionado && paisSeleccionado.nombre}
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-danger" onClick={() => eliminar()}>
+            Sí
+          </button>
+          <button
+            className="btn btn-secondary"
+            onClick={() => setModalEliminar(false)}
+          >
+            No
+          </button>
+        </ModalFooter>
+      </Modal>
+
+      <Modal isOpen={modalInsertar}>
+        <ModalHeader>
+          <div>
+            <h3>Insertar País</h3>
+          </div>
+        </ModalHeader>
+        <ModalBody>
+          <div className="form-group">
+            <label>País</label>
+            <input
+              className="form-control"
+              type="text"
+              name="nombre"
+              value={paisSeleccionado ? paisSeleccionado.nombre : ""}
+              onChange={handleChange}
+            />
+            <br />
+
+            <label>Minutos</label>
+            <input
+              className="form-control"
+              type="text"
+              name="minutos"
+              value={paisSeleccionado ? paisSeleccionado.minutos : ""}
+              onChange={handleChange}
+            />
+            <br />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-primary" onClick={() => insertar()}>
+            Insertar
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={() => setModalInsertar(false)}
+          >
+            Cancelar
+          </button>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 }
